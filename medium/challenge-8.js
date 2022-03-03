b82508d5c34024f15695c100615e53068df6da1e
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-03-03 17:49:13 -0300

    challenge-9

---
+++ medium/challenge-8.js
@@ -0,0 +1,5 @@
+const vegetals = ['Repolho', 'Nabo', 'Rabanete', 'Cenoura', 'Abobrinha'];
+
+vegetals.forEach((element, index) => {
+  console.log(`indice: ${index} - valor: ${element}`);
+});
---
+++ medium/challenge-9.js
@@ -0,0 +1,4 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const newNumbers = numbers.map((item) => item * 2);
+console.log(`Numbers dobro:  ${newNumbers}`);
