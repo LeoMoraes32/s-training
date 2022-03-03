3f4fe14ed6b1b93926c5150971a5adfbf3339a70
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-03-03 11:33:39 -0300

    config of eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'linebreak-style': 'off',
+    'class-methods-use-this': 'off'
+  }
+};
