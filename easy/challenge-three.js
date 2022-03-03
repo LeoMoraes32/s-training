f48574f4fb12febfd1d2b871b842c5b41aa60f12
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-03-03 15:34:35 -0300

    challenge three

---
+++ easy/challenge-three.js
@@ -0,0 +1,3 @@
+const vegetals = ['Repolho', 'Nabo', 'Rabanete', 'Cenoura'];
+let indexOf = vegetals.indexOf('Cenoura');
+console.log(`Indice de Cenoura: ${indexOf}`);
--- easy/challenge-two.js
+++ easy/challenge-two.js
@@ -1,6 +1,6 @@
 const vegetals = ['Repolho', 'Nabo', 'Rabanete', 'Cenoura'];
-
 let size = vegetals.length;
+
 console.log('Primeiro elemento do vetor: ' + vegetals[size - size]);
 console.log('Último elemento do vetor: ' + vegetals[size - 1]);
 console.log('Tamanho do vetor: ' + size);
