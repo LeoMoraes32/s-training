06e65fbecd6aaf69ff859f4a8c665fc978a6c0e5
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-03-03 16:16:47 -0300

    challenge-4

---
+++ easy/challenge-4.js
@@ -0,0 +1,4 @@
+const vegetals = ['Repolho', 'Nabo', 'Rabanete', 'Cenoura'];
+for (let index = 0; index < vegetals.length; index++) {
+  console.log(`indice: ${index} - valor: ${vegetals[index]}`);
+}
