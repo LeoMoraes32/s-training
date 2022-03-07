b8ca97432c77bc9636483c69388d2199aa4b4bbc
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-03-07 16:42:33 -0300

    refactor(challenge-18/post): add new verification in the enter of body

--- advanced/challenge-18.js
+++ advanced/challenge-18.js
@@ -18,12 +18,26 @@ const people = [
 
 //Rotas
 app.get('/', (req, res) => {
-  console.log('GET');
   return res.send(people);
 });
 
+app.get('/:id', (req, res) => {
+  if (req.params.id) {
+    const id = req.params.id;
+    return res.send(people[id]);
+  }
+});
 app.post('/', (req, res) => {
-  people.push(req.body);
-  res.status(201);
-  return res.send(req.body);
+  const body = Object.keys(req.body);
+  const peopleMock = Object.keys(people[0]);
+  const bodyString = JSON.stringify(body);
+  const peopleMockString = JSON.stringify(peopleMock);
+  if (bodyString === peopleMockString) {
+    people.push(req.body);
+    res.status(201);
+    return res.send(req.body);
+  } else {
+    res.status(400);
+    return res.send({ error: 'Wrong format' });
+  }
 });
