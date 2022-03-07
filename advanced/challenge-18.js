4517d0711b436354066eb1bde4d5bed27a22125c
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-03-07 18:21:26 -0300

    featr: check if exists and update

--- advanced/challenge-18.js
+++ advanced/challenge-18.js
@@ -27,6 +27,7 @@ app.get('/:id', (req, res) => {
     return res.send(people[id]);
   }
 });
+
 app.post('/', (req, res) => {
   const body = Object.keys(req.body);
   const peopleMock = Object.keys(people[0]);
@@ -39,5 +40,29 @@ app.post('/', (req, res) => {
   } else {
     res.status(400);
     return res.send({ error: 'Wrong format' });
+  }
+});
+
+app.put('/:id', (req, res) => {
+  const bodyMock = Object.keys(req.body);
+  const peopleMock = Object.keys(people[0]);
+
+  const bodyMockString = JSON.stringify(bodyMock);
+  const peopleMockString = JSON.stringify(peopleMock);
+
+  const body = req.body;
+  let result = '';
+  if (req.params.id && req.params.id <= people.length - 1) {
+    if (bodyMockString.length <= peopleMockString.length) {
+      const id = req.params.id;
+      result = people[id];
+
+      people[req.params.id] = body;
+      res.status(200);
+      return res.send(people[req.params.id]);
+    }
+  } else {
+    res.status(400);
+    return res.send({ error: 'Id not found' });
   }
 });
